abe19937623dc02223bc784663a84ad89386035c
fuwenchao <miniimg@163.com>
2023-04-04 15:38:02 +0800

    add copy file for build

--- setup/buil4win.bat
+++ setup/buil4win.bat
@@ -1,5 +1,6 @@
 @echo Create setup...
 @echo off
+copy ..\out\release\FlyTerm.exe .\packages\cn.flysnow.flyterm\data\FlyTerm.exe /Y
 d:\Qt\QtIFW-3.2.2\bin\binarycreator.exe -c .\config\config.xml -p .\packages .\release\FlyTerm1.0-win-x64_%date:~0,4%%date:~5,2%%date:~8,2%.exe
 @echo Create setup OK
 @echo Compress setup...
