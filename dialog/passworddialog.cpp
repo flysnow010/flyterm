68bc0ae75064b9b4099c36c009f69a4826593972
fuwenchao <miniimg@163.com>
2023-07-07 13:19:17 +0800

    hide help button

--- dialog/passworddialog.cpp
+++ dialog/passworddialog.cpp
@@ -6,6 +6,7 @@ PasswordDialog::PasswordDialog(QWidget *parent) :
     ui(new Ui::PasswordDialog)
 {
     ui->setupUi(this);
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
 }
 
 PasswordDialog::~PasswordDialog()
