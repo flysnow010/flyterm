52e33c238dc9aa82d4511b814e0b70e326d9a41b
fuwenchao <miniimg@163.com>
2023-08-17 19:27:49 +0800

    update color

--- color/consolecolor.cpp
+++ color/consolecolor.cpp
@@ -5,7 +5,7 @@ ConsoleColorManager::ConsoleColorManager()
     colorNames_ << "Light yellow Black";//浅黄背景黑字
     colors_ << ConsoleColor{ QColor(255, 255, 221), QColor(28, 28, 28) };
     colorNames_ << "White Black";//白底黑字
-    colors_ << ConsoleColor{ QColor(236, 236, 236), QColor(28, 28, 28) };
+    colors_ << ConsoleColor{ QColor(255, 255, 255), QColor(28, 28, 28) };
     colorNames_ << "Black Gray";//黑底灰字
     colors_ << ConsoleColor{ QColor(28, 28, 28), QColor(170, 170, 170) };
     colorNames_ << "Black Green";//黑底绿字
