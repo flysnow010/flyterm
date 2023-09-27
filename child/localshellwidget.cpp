d75694d72d6eddb1d40a4de870f355106435f6fc
fuwenchao <miniimg@163.com>
2023-09-27 14:48:26 +0800

    updte LocalShellWidget derive form Child

--- child/localshellwidget.cpp
+++ child/localshellwidget.cpp
@@ -1,26 +1,20 @@
 ﻿#include "localshellwidget.h"
 #include "console/localshellconsole.h"
-#include "core/commandthread.h"
 #include "core/localshell.h"
 #include "core/localshellsettings.h"
-#include "highlighter/hightlightermanager.h"
 #include "util/util.h"
 
-#include <QPrinter>
-#include <QPrintDialog>
-#include <QMenu>
-#include <QResizeEvent>
-#include <QTimer>
-
 LocalShellWidget::LocalShellWidget(bool isLog, QWidget *parent)
-    : QWidget(parent)
-    , console(new LocalShellConsole(this))
-    , commandThread(new CommandThread(this))
+    : Child(parent)
     , shell(new LocalShell(this))
-    , dataTimer(new QTimer(this))
 {
-    setAttribute(Qt::WA_DeleteOnClose);
-    console->createParserAndConnect();
+    createConsoleAndConnect();
+    connect(shell, SIGNAL(onData(QByteArray)), this, SLOT(onData(QByteArray)));
+    connect(console(), SIGNAL(onCommand(QString)), this, SLOT(setCommand(QString)));
+    connect(this, &Child::onClose, this, [=](QWidget *){
+        shell->stop();
+    });
+
     if(isLog)
     {
         beforeLogfile_ = LogFile::SharedPtr(new LogFile());
@@ -29,21 +23,6 @@ LocalShellWidget::LocalShellWidget(bool isLog, QWidget *parent)
                        .arg(uint64_t(this), 8, 16)
                        .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd HH-mm-ss")));
     }
-    connect(dataTimer, SIGNAL(timeout()), this, SLOT(pullData()));
-    connect(commandThread, SIGNAL(onAllCommand(QString)), this, SIGNAL(onCommand(QString)));
-    connect(commandThread, SIGNAL(onCommand(QString)), this, SLOT(execCommand(QString)));
-    connect(commandThread, SIGNAL(onExpandCommand(QString)),
-            this, SLOT(execExpandCommand(QString)), Qt::BlockingQueuedConnection);
-    connect(commandThread, SIGNAL(onTestCommand(QString)),
-            this, SLOT(execTestCommand(QString)));
-    connect(shell, SIGNAL(onData(QByteArray)), this, SLOT(onData(QByteArray)));
-    connect(console, SIGNAL(onGotCursorPos(int,int)), this, SLOT(onGotCursorPos(int,int)));
-    connect(console, SIGNAL(getData(QByteArray)), this, SLOT(writeData(QByteArray)));
-    connect(console, SIGNAL(onCommand(QString)), this, SLOT(setCommand(QString)));
-    connect(console, &QWidget::customContextMenuRequested,
-            this, &LocalShellWidget::customContextMenu);
-    commandThread->start();
-    dataTimer->start(1);
 }
 
 bool LocalShellWidget::runShell(LocalShellSettings const& settings)
@@ -57,8 +36,8 @@ bool LocalShellWidget::runShell(LocalShellSettings const& settings)
 
 bool LocalShellWidget::runCmdShell(LocalShellSettings const& settings)
 {
-    console->setLocalEchoEnabled(true);
-    console->setNeedNewLine(true);
+    console()->setLocalEchoEnabled(true);
+    console()->setNeedNewLine(true);
     QStringList params;
     if(!settings.startupPath.isEmpty())
         shell->setWorkingDirectory(settings.getCurrentPath());
@@ -66,6 +45,7 @@ bool LocalShellWidget::runCmdShell(LocalShellSettings const& settings)
         params << "/k" << settings.executeCommand.split(" ");
     shell->start(settings.shellType, params);
     isConnected_ = true;
+    display();
     return true;
 }
 
@@ -81,28 +61,13 @@ bool LocalShellWidget::runPowerShell(LocalShellSettings const& settings)
 
     shell->start(settings.shellType, params);
     isConnected_ = true;
+    display();
     return true;
-}
-
-void LocalShellWidget::sendCommand(QString const& command)
-{
-    QStringList commands = command.split("\n");
-    sendCommands(commands);
-}
-
-QSize LocalShellWidget::sizeHint() const
-{
-    return QSize(400, 300);
-}
-
-bool LocalShellWidget::isConnected() const
-{
-    return isConnected_;
 }
 
 void LocalShellWidget::reconnect(LocalShellSettings const& settings)
 {
-    console->clearall();
+    clearScrollback();
     runShell(settings);
 }
 
@@ -112,118 +77,14 @@ void LocalShellWidget::disconnect()
     isConnected_ = false;
 }
 
-bool LocalShellWidget::isDisplay() const
+Console* LocalShellWidget::createConsole()
 {
-    return dataTimer->isActive();
+    return new LocalShellConsole(this);
 }
 
-void LocalShellWidget::display()
+void LocalShellWidget::writeToShell(QByteArray const& data)
 {
-    dataTimer->start(1);
-}
-
-void LocalShellWidget::undisplay()
-{
-    dataTimer->stop();
-}
-
-void LocalShellWidget::setCodecName(QString const& name)
-{
-    console->setCodecName(name);
-}
-
-void LocalShellWidget::setFontName(QString const& name)
-{
-    console->setFontName(name);
-}
-
-void LocalShellWidget::setConsoleColor(ConsoleColor const& color)
-{
-   console->setConsoleColor(color);
-}
-
-void LocalShellWidget::setConsolePalette(ConsolePalette::Ptr palette)
-{
-    console->setConsolePalette(palette);
-}
-
-void LocalShellWidget::setFontSize(int fontSize)
-{
-    console->setFontSize(fontSize);
-}
-
-void LocalShellWidget::print()
-{
-    QPrinter printer(QPrinter::HighResolution);
-    QPrintDialog *dlg = new QPrintDialog(&printer, this);
-    if (console->textCursor().hasSelection())
-        dlg->addEnabledOption(QAbstractPrintDialog::PrintSelection);
-    dlg->setWindowTitle(tr("Print Document"));
-    if (dlg->exec() == QDialog::Accepted)
-        console->print(&printer);
-    delete dlg;
-}
-
-void LocalShellWidget::updateHightLighter(QString const& hightLighter)
-{
-    console->updateHightLighter(hightLighter);
-}
-
-void LocalShellWidget::setHighLighter(QString const& hightLighter)
-{
-    highLight_ = hightLighter;
-}
-
-
-void LocalShellWidget::save()
-{
-    console->saveToFile();
-}
-
-void LocalShellWidget::copy()
-{
-    console->copyOne();
-}
-
-void LocalShellWidget::copyAll()
-{
-    console->copyAll();
-}
-
-void LocalShellWidget::paste()
-{
-    console->paste();
-}
-
-void LocalShellWidget::increaseFontSize()
-{
-    if(console->increaseFontSize())
-        emit fontSizeChanged(console->fontSize());
-}
-void LocalShellWidget::decreaseFontSize()
-{
-    if(console->decreaseFontSize())
-        emit fontSizeChanged(console->fontSize());
-}
-
-void LocalShellWidget::clearScrollback()
-{
-    console->clearall();
-}
-
-void LocalShellWidget::resizeEvent(QResizeEvent *event)
-{
-    console->resize(event->size());
-}
-
-void LocalShellWidget::closeEvent(QCloseEvent *event)
-{
-    emit onClose(this);
-    event->accept();
-    shell->stop();
-    commandThread->stop();
-    commandThread->wait();
-    commandThread->quit();
+    shell->write(data);
 }
 
 void LocalShellWidget::onData(QByteArray const& data)
@@ -242,88 +103,7 @@ void LocalShellWidget::writeData(QByteArray const&data)
     shell->write(data);
 }
 
-void LocalShellWidget::customContextMenu(const QPoint &)
-{
-    QMenu contextMenu;
-
-    contextMenu.addAction(tr("Copy"), this, SLOT(copy()));
-    contextMenu.addAction(tr("Copy All"), this, SLOT(copyAll()));
-    contextMenu.addAction(tr("Paste"), this, SLOT(paste()));
-    contextMenu.addSeparator();
-    contextMenu.addAction(tr("Increase Font Size"), this, SLOT(increaseFontSize()));
-    contextMenu.addAction(tr("Decrease Font Size"), this, SLOT(decreaseFontSize()));
-    contextMenu.addSeparator();
-    createHighLightMenu(contextMenu.addMenu(tr("Syntax Highlighting")));
-    contextMenu.addSeparator();
-    contextMenu.addAction(tr("Save To File..."), this, SLOT(save()));
-    contextMenu.addAction(tr("Clear Scrollback"), this, SLOT(clearScrollback()));
-
-    contextMenu.exec(QCursor::pos());
-    console->cancelSelection();
-}
-
-void LocalShellWidget::setHighLighter()
-{
-    QAction* actoin = (QAction*)sender();
-    if(actoin)
-        emit highLighterChanged(actoin->data().toString());
-}
-
-void LocalShellWidget::onGotCursorPos(int row, int col)
-{
-    QString cursorPos = QString("\033[%1;%2R").arg(row).arg(col);
-    shell->write(cursorPos.toUtf8());
-}
-
-void LocalShellWidget::execCommand(QString const& command)
-{
-    shell->write(QString("%1\n").arg(command).toUtf8());
-}
-
-void LocalShellWidget::execTestCommand(QString const& command)
-{
-    QString testCommand = command.right(command.size() - 1);
-    if(!testCommand.startsWith("start"))
-        testCommands_.push_back(testCommand);
-    else
-    {
-        QStringList cmds = testCommand.split(' ');
-        if(cmds.size() > 1)
-            testParam_ = cmds[1].toUtf8();
-        isTest_ = true;
-        QString cmd = getTestCommand();
-        if(!cmd.startsWith("#"))
-            execCommand(cmd);
-        else
-        {
-            execExpandCommand(cmd);
-            execCommand(QString());
-        }
-    }
-}
-
-void LocalShellWidget::execExpandCommand(QString const& command)
-{
-    QStringList cmds = command.split(' ');
-    if(command.startsWith("#bsave"))
-    {
-        if(cmds.size() > 1)
-        {
-            LogFile::SharedPtr logfile(new LogFile());
-            if(logfile->open(cmds[1], false))
-            {
-                afterLogfile_ = logfile;
-                console->setLogFile(afterLogfile_);
-            }
-        }
-    }
-    else if(command.startsWith("#esave"))
-    {
-        afterLogfile_ = LogFile::SharedPtr();
-    }
-}
-
-void LocalShellWidget::pullData()
+void LocalShellWidget::onPullData()
 {
     if(texts.isEmpty())
         return;
@@ -331,75 +111,9 @@ void LocalShellWidget::pullData()
     QString data;
     while(texts.size() > 0 && data.size() < 512)
         data.push_back(texts.takeFirst());
-
-    if(isTest_)
-    {
-        testData_.push_back(data.toUtf8());
-        if(testData_.contains(testParam_))
-        {
-            QString command = getTestCommand();
-            if(!command.startsWith("#"))
-                execCommand(command);
-            else
-            {
-                execExpandCommand(command);
-                execCommand(QString());
-            }
-            testData_.clear();
-            if(testCommandIsEmpty())
-                isTest_ = false;
-        }
-    }
-
+    onDisplay(data.toUtf8());
     if(beforeLogfile_)
         beforeLogfile_->write(data);
-    console->putData(data.toUtf8());
-}
-
-void LocalShellWidget::createHighLightMenu(QMenu* menu)
-{
-    emit getHighlighter();
-    for(int i = 0; i < highLighterManager->size(); i++)
-    {
-        HighLighterManager::HighLighter const& lighter = highLighterManager->highLighter(i);
-        QAction *action = menu->addAction(lighter.text, this, SLOT(setHighLighter()));
-        action->setData(lighter.name);
-        action->setCheckable(true);
-        if(highLight_ == lighter.name)
-            action->setChecked(true);
-    }
-}
-
-void LocalShellWidget::sendCommands(QStringList const& commands)
-{
-    for(int i = 0; i < commands.size(); i++)
-    {
-        QString command = commands.at(i);
-        if(command.isEmpty())
-            continue;
-        commandThread->postCommand(command);
-    }
-}
-
-QString LocalShellWidget::getTestCommand()
-{
-    if(testCommands_.isEmpty())
-        return QString();
-    QString command = testCommands_.front();
-    testCommands_.pop_front();
-    return command;
-}
-
-bool LocalShellWidget::testCommandIsEmpty() const
-{
-    return testCommands_.isEmpty();
-}
-
-QByteArray LocalShellWidget::toUnicode(QString const& text)
-{
-    return QByteArray::fromRawData(
-                reinterpret_cast<const char *>(text.utf16()),
-                text.size() * sizeof(ushort));
 }
 
 void LocalShellWidget::setCommand(QString const& command)
